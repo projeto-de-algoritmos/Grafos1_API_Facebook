660efd9b590d82e8db76b0b5920bb6a39b702f68
Juliana Valle <juju.pvg@gmail.com>
2022-02-07 18:26:41 -0300

    criando aplicação web mvc
    Co-authored-by: Matheus Monteiro <matheusyanmonteiro@gmail.com>

---
+++ graphFacebook/graphFacebookWeb/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using graphFacebookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace graphFacebookWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly string apiUrl = "https://localhost:44389";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Home> listaPessoas = new List<Home>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(apiUrl))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    //listaPessoas = JsonConvert.DeserializeObject<List<Home>>(apiResponse);
+                }
+            }
+            return View(listaPessoas);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
