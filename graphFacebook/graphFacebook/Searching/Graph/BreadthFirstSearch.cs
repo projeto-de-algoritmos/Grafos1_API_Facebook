15841bb9c4d8e7eaad99138f65875c356ed48225
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-02-07 17:13:46 -0300

    criando algoritmo BFS de busca no grafo
    Co-authored-by: Juliana Valle <juju.pvg@gmail.com>

--- graphFacebook/graphFacebook/Searching/Graph/BreadthFirstSearch.cs
+++ graphFacebook/graphFacebook/Searching/Graph/BreadthFirstSearch.cs
@@ -1,9 +1,42 @@
-﻿
+﻿using Cnsl.DataStructures;
 
 namespace graphFacebook.Searching.Graph
 {
     public class BreadthFirstSearch
     {
-        
+
+        public static GraphSearchResult Search(IGraph graph, IVertex source, IVertex target)
+        {
+            if (graph is null)
+                throw new ArgumentNullException(nameof(graph));
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+            if (target is null)
+                throw new ArgumentNullException(nameof(target));
+
+            var result = new GraphSearchResult(graph.Count);
+            var queue = new Queue<IVertex>(graph.Count);
+
+            result.MarkAsVisited(source);
+            queue.Enqueue(source);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                if (vertex == target)
+                    break;
+
+                foreach (var edge in vertex.Edges)
+                {
+                    if (!result.IsVisited(edge.U))
+                    {
+                        result.MarkAsVisited(edge.U);
+                        queue.Enqueue(edge.U);
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
