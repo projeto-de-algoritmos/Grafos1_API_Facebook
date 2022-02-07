f24aaf07bf771203815119135a13ee58bd48fb23
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-02-07 17:41:53 -0300

    Recriando estrutura de dados estáticos.
    Co-authored-by: Juliana Valle <juju.pvg@gmail.com>

--- graphFacebook/graphFacebook/Data/StaticData.cs
+++ graphFacebook/graphFacebook/Data/StaticData.cs
@@ -5,15 +5,40 @@ namespace graphFacebook.Data
     public class StaticData
     {
         public List<User> UserStaticData { get; set; }
+
+        User persona1 = new User("", "", new List<int> { });
+        User persona2 = new User("", "", new List<int> { });
+        User persona3 = new User("", "", new List<int> { });
+        User persona4 = new User("", "", new List<int> { });
+        User persona5 = new User("", "", new List<int> { });
+
         public StaticData()
         {
-            UserStaticData = new List<User>();
-            var persona1 = new User("Fulano de tal", "", new List<int> { });
-            var persona2 = new User("Fulano de tal", "", new List<int> { persona1.Id });
-            var persona3 = new User("Fulano de tal", "", new List<int> { });
-            var persona4 = new User("Fulano de tal", "", new List<int> { });
-            var persona5 = new User("Fulano de tal", "", new List<int> {persona3.Id, persona1.Id });
 
+            persona1.Nome = "teste";
+            persona1.Idade = "teste";
+            persona1.Conexoes = new List<int> { persona2.Id, persona3.Id, persona4.Id };
+
+            persona2.Nome = "teste";
+            persona2.Idade = "teste";
+            persona2.Conexoes = new List<int> { persona1.Id, persona3.Id };
+
+            persona3.Nome = "teste";
+            persona3.Idade = "teste";
+            persona3.Conexoes = new List<int> { persona1.Id, persona2.Id, persona5.Id };
+
+            persona4.Nome = "teste";
+            persona4.Idade = "teste";
+            persona4.Conexoes = new List<int> { persona1.Id, persona5.Id };
+
+            persona5.Nome = "teste";
+            persona5.Idade = "teste";
+            persona5.Conexoes = new List<int> { persona4.Id, persona3.Id };
+
+
+
+            UserStaticData = new List<User>();
+           
             UserStaticData.Add(persona1);
             UserStaticData.Add(persona2);
             UserStaticData.Add(persona3);
